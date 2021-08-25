026a4369e03659fa42a3f25f238663d33bf17656
Arkajit <59366904+Arkajit19@users.noreply.github.com>
2021-08-25 12:13:22 +0530

    Add files via upload

---
+++ Lotus.java
@@ -0,0 +1,12 @@
+package fim.otam;
+
+public class Lotus {
+
+    public static void main(String[] args) {
+        String strs="Mumbai";
+        StringBuffer stbf=new StringBuffer(strs);
+        System.out.println("The old word is : "+strs);
+        System.out.println("The new word is : "+stbf.reverse());
+    }
+
+}
