92f8003da6adc4f321220f191e059527f94be64e
Arkajit <59366904+Arkajit19@users.noreply.github.com>
2021-08-26 13:39:24 +0530

    Add files via upload

---
+++ Fader.java
@@ -0,0 +1,13 @@
+package field;
+import java.util.*;
+public class Fader {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the first number : ");
+        double d=input.nextDouble();
+        System.out.println("Enter the first number : ");
+        double d1=input.nextDouble();
+        double d2=d*d1;
+        System.out.println("The result is : "+d2);
+    }
+}
