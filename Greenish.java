a975cd4f687c52f004f6da2b7d64369192adc5ae
Arkajit <59366904+Arkajit19@users.noreply.github.com>
2021-08-26 15:06:19 +0530

    Add files via upload

---
+++ Greenish.java
@@ -0,0 +1,32 @@
+package field;
+import java.util.*;
+import java.math.*;
+import java.lang.*;
+import java.io.*;
+public class Greenish {
+    public static void main(String[] args) {
+
+        System.out.println("ax2+bx+c=0");
+       Scanner input=new Scanner(System.in);
+        System.out.println("The value of a is : ");
+        double a=input.nextDouble();
+        System.out.println("The value of b is : ");
+        double b=input.nextDouble();
+        System.out.println("The value of c is : ");
+        double c=input.nextDouble();
+        double k1=(Math.pow(b,2)-(4*a*c));
+        double k2=(2*a);
+        double x1= ((-b)+Math.sqrt(k1))/k2;
+        double x2= ((-b)-Math.sqrt(k1))/k2;
+        if (k1>0||k1==0){
+
+            System.out.println("One root is : "+x1);
+            System.out.println("Another root is "+x2);
+        }
+        else {
+
+            System.out.println("The roots are not real");
+        }
+
+    }
+}
