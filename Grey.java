4e57362e076f866f68c27030c6dd6af435ee25b5
Arkajit <59366904+Arkajit19@users.noreply.github.com>
2021-08-26 15:53:26 +0530

    Add files via upload

---
+++ Grey.java
@@ -0,0 +1,18 @@
+package field;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Grey {
+    public static void main(String[] args) {
+        List<String>listcolor=new ArrayList<>();
+        listcolor.add("Red");
+        listcolor.add("Blue");
+        listcolor.add("Black");
+        listcolor.add("Orange");
+        listcolor.add("Yellow");
+        for (String r: listcolor){
+
+            System.out.println(r);
+        }
+    }
+}
