552be8b6409c8453cfdd2ea305f9b6a69bf151b2
Arkajit <59366904+Arkajit19@users.noreply.github.com>
2021-08-26 20:47:04 +0530

    Add files via upload

---
+++ Faram.java
@@ -0,0 +1,22 @@
+package lit.votom;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Faram {
+    public static void main(String[] args) {
+
+        List<Integer> list=new ArrayList<>();
+        list.add(67);
+        list.add(23);
+        list.add(17);
+        list.add(10);
+        list.add(87);
+        list.add(29);
+        System.out.println(list);
+        for (int k: list){
+
+            System.out.print(k+" ");
+        }
+    }
+}
