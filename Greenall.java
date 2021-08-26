4534512ae9e20282cf9d5cc03a03ac9aee8d9a91
Arkajit <59366904+Arkajit19@users.noreply.github.com>
2021-08-26 21:48:30 +0530

    Add files via upload

---
+++ Greenall.java
@@ -0,0 +1,8 @@
+package lit.votom;
+
+public class Greenall {
+    public static void main(String[] args) {
+        Orange org=new Orange();
+        org.polo();
+    }
+}
---
+++ Orange.java
@@ -0,0 +1,22 @@
+package lit.votom;
+import java.util.*;
+public class Orange {
+
+    public void polo(){
+
+        List<Integer>list_num=new ArrayList<>();
+        list_num.add(45);
+        list_num.add(23);
+        list_num.add(78);
+        list_num.add(19);
+        list_num.add(20);
+        list_num.add(0,67);
+        for (int r: list_num){
+
+            System.out.print(r+" ");
+        }
+        System.out.println();
+
+    }
+
+}
