4dc5e6d96e3bb8f8c1f0853a73d2f2555f328447
Arkajit <59366904+Arkajit19@users.noreply.github.com>
2021-09-13 17:16:45 +0530

    Add files via upload

---
+++ Longpop.java
@@ -0,0 +1,15 @@
+package pi.ola;
+
+import java.util.StringTokenizer;
+
+public class Longpop {
+    public static void main(String[] args) {
+
+        String lineone="My name is Arkajit";
+        StringTokenizer strtok=new StringTokenizer(lineone);
+        while (strtok.hasMoreElements()){
+
+            System.out.println(strtok.nextToken());
+        }
+    }
+}
