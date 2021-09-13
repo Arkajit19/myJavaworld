22e0b7059a9226f8ab6900608bc467a6c91dc748
Arkajit <59366904+Arkajit19@users.noreply.github.com>
2021-09-13 17:20:30 +0530

    Update Longpop.java

--- Longpop.java
+++ Longpop.java
@@ -6,7 +6,7 @@ public class Longpop {
     public static void main(String[] args) {
 
         String lineone="My name is Arkajit";
-        StringTokenizer strtok=new StringTokenizer(lineone);
+        StringTokenizer strtok=new StringTokenizer(lineone," ");
         while (strtok.hasMoreElements()){
 
             System.out.println(strtok.nextToken());
